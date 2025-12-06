5a12824f0cd2b325282ad857ec07042e5b3ea722
3xcpy <3xcpyy@gmail.com>
2025-12-06 16:50:43 +0100

    randomize enemy spawning

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
 @export var enemy_scene: PackedScene = null
-@export var spawn_delay: float = 2.0
+@export var min_spawn_delay: float = 2.0
+@export var max_spawn_delay: float = 10.0
 
+var spawn_delay: float = 0.0
 var timer: float = 0.0
+
+
+func _ready() -> void:
+	compute_spawn_delay()
 
 
 func _physics_process(delta: float) -> void:
@@ -11,9 +17,14 @@ func _physics_process(delta: float) -> void:
 	if timer >= spawn_delay:
 		spawn()
 		timer = 0.0
+		compute_spawn_delay()
 
 
 func spawn() -> void:
 	var enemy: Node2D = enemy_scene.instantiate()
 	add_child(enemy)
 	enemy.global_position = global_position
+
+
+func compute_spawn_delay() -> void:
+	spawn_delay = randf_range(min_spawn_delay, max_spawn_delay)
